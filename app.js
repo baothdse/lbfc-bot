dd4dc332b4ec2b80f71e00f8282d6bc9167f91c8
DESKTOP-30B8PLJ\BAOTHDSE61666 <baothdse@gmail.com>
2017-08-31 20:36:34 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+//morgan
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+app.listen(port);
+
+app.get('/', function(req, res) {
+    res.send('Facebook Chatbot Here'); 
+});
+
+app.get('/webhook', function(req, res) {
+    if(req.query["hub.verify_token"] === "this_is_my_token") {
+        console.log("Verified Webhook");
+        res.status(200).send(req.query["hub-challenge"]);
+    } else {
+        console.log("Verification failed. The token do not match");
+        res.sendStatus(403);
+    }
+});
+
+console.log("Server start on port " + port);
