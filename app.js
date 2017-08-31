8f6b714ba34c30cd859083cbe65f34833effbc37
DESKTOP-30B8PLJ\BAOTHDSE61666 <baothdse@gmail.com>
2017-08-31 21:14:34 +0700

    Fix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var morgan = require('morgan');
 var app = express();
 
 var port = process.env.PORT || 3000;
+var token = process.env.VERIFICATION_TOKEN;
 //morgan
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
@@ -19,14 +20,14 @@ app.get('/', function(req, res) {
     res.send('Facebook Chatbot Here'); 
 });
 
-// app.get('/webhook', function(req, res) {
-//     if(req.query["hub.verify_token"] === "this_is_my_token") {
-//         console.log("Verified Webhook");
-//         res.status(200).send(req.query["hub-challenge"]);
-//     } else {
-//         console.log("Verification failed. The token do not match");
-//         res.sendStatus(403);
-//     }
-// });
+app.get('/webhook', function(req, res) {
+    if(req.query["hub.verify_token"] === token) {
+        console.log("Verified Webhook");
+        res.status(200).send(req.query["hub-challenge"]);
+    } else {
+        console.log("Verification failed. The token do not match");
+        res.sendStatus(403);
+    }
+});
 
 console.log("Server start on port " + port);
