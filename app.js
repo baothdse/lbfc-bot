c4e905efde3420ab754313bc894fbc14cdbff0af
DESKTOP-30B8PLJ\BAOTHDSE61666 <baothdse@gmail.com>
2017-09-04 12:17:21 +0700

    Fix

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ function sendMessage(event) {
     apiai.on('response', (response) => {
         console.log("API.AI is on response state");
         let aiText = response.result.fulfillment.speech;
-        console.log(response.result.fulfillment);
+        console.log(response);
         request({
             url: "https://graph.facebook.com/v2.6/me/messages",
             qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
