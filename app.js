cede8040e7755ba10ad04e7ba2de8734062ef824
baothdse <baothdse@gmail.com>
2017-10-13 00:04:16 +0700

    Fix

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.post('/webhook', function (req, res) {
             var senderId = message.sender.id;
             if (message.message) {
                 //if user send text
-                if(message.message.text) {
+                if (message.message.text) {
                     console.log('Here you are')
                     bot.reply(senderId, message.message.text)
                 }
@@ -102,3 +102,84 @@ console.log('Server start on port: ' + port)
 //         }
 //     }
 // };
+
+function addPersistentMenu() {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+        qs: { access_token: process.env.FB_TOKEN },
+        method: 'POST',
+        json: {
+            "get_started": {
+                "payload": "GET_STARTED_PAYLOAD"
+            }
+        }
+    }, function (error, response, body) {
+        console.log("Add persistent menu " + response)
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+        qs: { access_token: process.env.FB_TOKEN },
+        method: 'POST',
+        json: {
+            "persistent_menu": [
+                {
+                    "locale": "default",
+                    "composer_input_disabled": true,
+                    "call_to_actions": [
+                        {
+                            "title": "Home",
+                            "type": "postback",
+                            "payload": "HOME"
+                        },
+                        {
+                            "title": "Nested Menu Example",
+                            "type": "nested",
+                            "call_to_actions": [
+                                {
+                                    "title": "Who am I",
+                                    "type": "postback",
+                                    "payload": "WHO"
+                                },
+                                {
+                                    "title": "Joke",
+                                    "type": "postback",
+                                    "payload": "joke"
+                                },
+                                {
+                                    "title": "Contact Info",
+                                    "type": "postback",
+                                    "payload": "CONTACT"
+                                }
+                            ]
+                        },
+                        {
+                            "type": "web_url",
+                            "title": "Latest News",
+                            "url": "http://foxnews.com",
+                            "webview_height_ratio": "full"
+                        }
+                    ]
+                },
+                {
+                    "locale": "zh_CN",
+                    "composer_input_disabled": false
+                }
+            ]
+        }
+
+    }, function (error, response, body) {
+        console.log(response)
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+addPersistentMenu()
