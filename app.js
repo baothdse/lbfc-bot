573b15cf28285b7a16ca46eb8bab097c08eb301e
baothdse <baothdse@gmail.com>
2017-10-13 00:40:52 +0700

    fix

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ function addPersistentMenu() {
                             "title": "Trang chủ",
                             "type": "web_url",
                             "url": "http://tiki.vn",
-                            "webview_height_ratio": "tall"
+                            "webview_height_ratio": "compact"
                         },
                         {
                             "title": "Về chúng tôi",
@@ -183,25 +183,25 @@ function addPersistentMenu() {
     })
 }
 
-// function removePersistentMenu() {
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-//         qs: { access_token: process.env.FB_TOKEN },
-//         method: 'POST',
-//         json: {
-//             setting_type: "call_to_actions",
-//             thread_state: "existing_thread",
-//             call_to_actions: []
-//         }
+function removePersistentMenu() {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+        qs: { access_token: process.env.FB_TOKEN },
+        method: 'POST',
+        json: {
+            setting_type: "call_to_actions",
+            thread_state: "existing_thread",
+            call_to_actions: []
+        }
 
-//     }, function (error, response, body) {
-//         console.log(response)
-//         if (error) {
-//             console.log('Error sending messages: ', error)
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error)
-//         }
-//     })
-// }
-// removePersistentMenu()
+    }, function (error, response, body) {
+        console.log(response)
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+removePersistentMenu()
 addPersistentMenu()
