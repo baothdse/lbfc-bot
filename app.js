d5672581b5388bc6151c2802016b82897abe340d
baothdse <baothdse@gmail.com>
2017-10-13 00:36:36 +0700

    fix

--- app.js
+++ app.js
@@ -182,25 +182,25 @@ function addPersistentMenu() {
     })
 }
 
-function removePersistentMenu() {
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/thread_settings',
-        qs: { access_token: process.env.FB_TOKEN },
-        method: 'POST',
-        json: {
-            setting_type: "call_to_actions",
-            thread_state: "existing_thread",
-            call_to_actions: []
-        }
+// function removePersistentMenu() {
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+//         qs: { access_token: process.env.FB_TOKEN },
+//         method: 'POST',
+//         json: {
+//             setting_type: "call_to_actions",
+//             thread_state: "existing_thread",
+//             call_to_actions: []
+//         }
 
-    }, function (error, response, body) {
-        console.log(response)
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-removePersistentMenu()
+//     }, function (error, response, body) {
+//         console.log(response)
+//         if (error) {
+//             console.log('Error sending messages: ', error)
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error)
+//         }
+//     })
+// }
+// removePersistentMenu()
 addPersistentMenu()
